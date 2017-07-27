a01aab203cd79e4c826ae00157f24d24e27f25ee
benjamin-mortimer <benjamin.mortimer@digital.cabinet-office.gov.uk>
2017-07-27 16:19:58 +0100

    Make toggle text change when toggled

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 function showHide() {
     var x = document.getElementById('x');
+    var y = document.getElementById('toggle-button')
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        y.innerHtml = "Click again to make paragraph x reappear";
     } else {
         x.style.display = 'none';
+        y.innerHtml = "Click to make paragraph x disappear";
     }
 }
