ba02a93413eb112fdad6c06654c0f3a10f8bcd0a
benjamin-mortimer <benjamin.mortimer@digital.cabinet-office.gov.uk>
2017-07-27 14:46:05 +0100

    Add show-hide if function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-
+function showHide() {
+    var x = document.getElementById('x');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
