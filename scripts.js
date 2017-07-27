941894a5ca26ab06ee0614debb41144de498d764
benjamin-mortimer <benjamin.mortimer@digital.cabinet-office.gov.uk>
2017-07-27 16:42:58 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ function showHide() {
     var y = document.getElementById('toggle-button')
     if (x.style.display === 'none') {
         x.style.display = 'block'
-        y.innerhtml = 'Click to make paragraph x disappear'
+        y.innerHTML = 'Click to make paragraph x disappear'
     } else {
         x.style.display = 'none'
-        y.innerhtml = 'Click again to make paragraph x reappear'
+        y.innerHTML = 'Click again to make paragraph x reappear'
     }
 }
